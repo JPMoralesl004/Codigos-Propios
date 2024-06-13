6ca1bbeb5b84e7c40de6aaab68b4452bcd14853f
JP <144849667+JPMoralesl004@users.noreply.github.com>
2024-06-13 02:13:05 -0600

    Update CartaMedicina.java
    Ajustadas para manejar el nuevo atributo "color"

--- Posibles Examenes Final Progra 2/Virus/CartaMedicina.java
+++ Posibles Examenes Final Progra 2/Virus/CartaMedicina.java
@@ -1,11 +1,11 @@
 class CartaMedicina extends Carta {
-    public CartaMedicina(String nombre) {
-        super(nombre, "Medicina");
+    public CartaMedicina(String nombre, String color) {
+        super(nombre, "Medicina", color);
     }
 
     @Override
     public void jugar(Jugador jugador) {
-        System.out.println("Carta Medicina jugada: " + getNombre());
+        System.out.println("Carta Medicina jugada: " + getNombre() + " de color " + getColor());
         jugador.curarOrganos();
     }
 }
