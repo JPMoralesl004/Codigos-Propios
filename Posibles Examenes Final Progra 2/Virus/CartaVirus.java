541b3cc0ba4f3baba0782c5c28a393cf48578563
JP <144849667+JPMoralesl004@users.noreply.github.com>
2024-06-13 02:11:48 -0600

    Update CartaVirus.java
    se ajusta para el color de las cartas

--- Posibles Examenes Final Progra 2/Virus/CartaVirus.java
+++ Posibles Examenes Final Progra 2/Virus/CartaVirus.java
@@ -1,11 +1,11 @@
 class CartaVirus extends Carta {
-    public CartaVirus(String nombre) {
-        super(nombre, "Virus");
+    public CartaVirus(String nombre, String color) {
+        super(nombre, "Virus", color);
     }
 
     @Override
     public void jugar(Jugador jugador) {
-        System.out.println("Carta Virus jugada: " + getNombre());
+        System.out.println("Carta Virus jugada: " + getNombre() + " de color " + getColor());
         jugador.infectarOrganos();
     }
 }
