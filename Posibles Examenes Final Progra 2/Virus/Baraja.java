99196e181499e9b4ab0101fea4a38a8704545b5c
JP <144849667+JPMoralesl004@users.noreply.github.com>
2024-06-13 02:07:22 -0600

    Update Baraja.java
    Incluir cartas con colores en la baraja.

--- Posibles Examenes Final Progra 2/Virus/Baraja.java
+++ Posibles Examenes Final Progra 2/Virus/Baraja.java
@@ -11,14 +11,18 @@ class Baraja {
     }
 
     private void inicializarBaraja() {
-        cartas.add(new CartaVirus("Virus 1"));
-        cartas.add(new CartaVirus("Virus 2"));
-        cartas.add(new CartaMedicina("Medicina 1"));
-        cartas.add(new CartaMedicina("Medicina 2"));
-        cartas.add(new CartaOrgano("Corazón"));
-        cartas.add(new CartaOrgano("Riñón"));
-        cartas.add(new CartaTratamiento("Antivirales"));
-        cartas.add(new CartaTratamiento("Antibióticos"));
+        cartas.add(new CartaVirus("Virus 1", "Rojo"));
+        cartas.add(new CartaVirus("Virus 2", "Verde"));
+        cartas.add(new CartaMedicina("Medicina 1", "Azul"));
+        cartas.add(new CartaMedicina("Medicina 2", "Amarillo"));
+        cartas.add(new CartaOrgano("Corazón", "Rojo"));
+        cartas.add(new CartaOrgano("Riñón", "Azul"));
+        cartas.add(new CartaTratamiento("Antivirales", "Verde"));
+        cartas.add(new CartaTratamiento("Antibióticos", "Amarillo"));
+        cartas.add(new CartaVirus("Virus 3", "Azul"));
+        cartas.add(new CartaMedicina("Medicina 3", "Rojo"));
+        cartas.add(new CartaOrgano("Hígado", "Verde"));
+        cartas.add(new CartaTratamiento("Vacuna", "Rojo"));
     }
 
     public void barajar() {
