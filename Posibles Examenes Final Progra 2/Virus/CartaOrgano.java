13d22d68ee48678daaa0eb5e80768b72ef37e4ab
JP <144849667+JPMoralesl004@users.noreply.github.com>
2024-06-13 02:13:45 -0600

    Update CartaOrgano.java
    ajustada para manejar el nuevo atributo "color".

--- Posibles Examenes Final Progra 2/Virus/CartaOrgano.java
+++ Posibles Examenes Final Progra 2/Virus/CartaOrgano.java
@@ -1,11 +1,11 @@
 class CartaOrgano extends Carta {
-    public CartaOrgano(String nombre) {
-        super(nombre, "Organo");
+    public CartaOrgano(String nombre, String color) {
+        super(nombre, "Organo", color);
     }
 
     @Override
     public void jugar(Jugador jugador) {
-        System.out.println("Carta Órgano jugada: " + getNombre());
+        System.out.println("Carta Órgano jugada: " + getNombre() + " de color " + getColor());
         jugador.agregarOrgano(getNombre());
     }
 }
