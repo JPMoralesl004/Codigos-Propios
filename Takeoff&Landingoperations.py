147c4ff9d42e7cd2e700f1d1793609278a46bb50
JP <144849667+JPMoralesl004@users.noreply.github.com>
2024-10-07 12:49:44 +0200

    Create Takeoff&Landingoperations.py
    Primera parte de la traducción a ingles del código original

---
+++ Takeoff&Landingoperations.py
@@ -0,0 +1,18 @@
+def main():
+    while True:
+        print("Welcome to the system to calculate takeoffs and landing in an airplane Cessna 172N for internal flights in Guatemala")
+        eleccion = input("What calculation would you like to perform? \n1. Landing \n2. Takeoff \n3. Both Calculations \nChoose an option (1, 2, 3): ")
+
+        if eleccion not in ["1", "2", "3"]:
+            print("Please, choose a valid option.")
+            continue
+
+        if eleccion in ["1", "3"]:
+            print("\nTakeoff Calculation:")
+            show_airport_list()
+            departure_airport = input("Please, enter the departure airport/aerodrom indicator: ").upper()
+            try:
+                temperture = float(input("Please, enter the temperature of the departure airport (in Fahrenheit degrees): "))
+                headwind = float(input("Do you have headwind? (knots): "))
+                tailwind = float(input("How many knots of tailwind do you have? "))
+            except ValueError:
